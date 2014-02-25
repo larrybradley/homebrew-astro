2906dcccbc03a26b04fac61fdacdd31fcae37048
Larry Bradley <larry.bradley@gmail.com>
2014-02-25 13:46:46 -0500

    wcstools

--- wcstools.rb
+++ wcstools.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class LibwcsWcstools < Formula
+class Wcstools < Formula
   homepage 'http://tdc-www.harvard.edu/wcstools/'
   url 'http://tdc-www.harvard.edu/software/wcstools/wcstools-3.8.7.tar.gz'
   sha1 '412d22938b05110eb86f7b01a874c17447ad2f19'
