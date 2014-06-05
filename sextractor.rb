ded2d5fda1dddb12af7e8c1f94f0e2985b197f52
Larry Bradley <larry.bradley@gmail.com>
2014-06-05 15:50:08 -0400

    sextractor: use double quotes per style guide

--- sextractor.rb
+++ sextractor.rb
@@ -1,14 +1,14 @@
-require 'formula'
+require "formula"
 
 class Sextractor < Formula
-  homepage 'https://www.astromatic.net/software/sextractor'
-  url 'https://www.astromatic.net/download/sextractor/sextractor-2.8.6.tar.gz'
-  sha1 '103ac2d51d9bae9fcbc5dda3031d82cd127f8250'
+  homepage "https://www.astromatic.net/software/sextractor"
+  url "https://www.astromatic.net/download/sextractor/sextractor-2.8.6.tar.gz"
+  sha1 "103ac2d51d9bae9fcbc5dda3031d82cd127f8250"
 
-  depends_on 'fftw'
-  depends_on 'autoconf' => :build
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
+  depends_on "fftw"
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
 
   def patches
     # use Mac OSX Accelerate framework instead of ATLAS
@@ -24,11 +24,11 @@ class Sextractor < Formula
       --enable-dependency-tracking
     ]
 
-    system 'autoreconf -fi'
-    system 'glibtoolize -fi'
-    system './configure', *args
-    system 'make'
-    system 'make install'
-    mv Dir['config/*'], share+'sextractor'
+    system "autoreconf -fi"
+    system "glibtoolize -fi"
+    system "./configure", *args
+    system "make"
+    system "make install"
+    mv Dir["config/*"], share+"sextractor"
   end
 end
