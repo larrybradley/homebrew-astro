e6b9fde06b72e1d9afb2d5450a988579db8eb34a
Larry Bradley <larry.bradley@gmail.com>
2014-07-29 15:44:36 -0400

    Update tinytim formula

--- tinytim.rb
+++ tinytim.rb
@@ -1,11 +1,11 @@
-require 'formula'
+require "formula"
 
 class Tinytim < Formula
-  homepage 'http://tinytim.stsci.edu/'
-  url 'http://tinytim.stsci.edu/static/tinytim-7.5.tar.gz'
-  sha1 '98ead7af02a616b5da5ef5b93fb4b38710bf815b'
+  homepage "http://tinytim.stsci.edu/"
+  url "http://tinytim.stsci.edu/static/tinytim-7.5.tar.gz"
+  sha1 "98ead7af02a616b5da5ef5b93fb4b38710bf815b"
 
-  option 'without-threaded', 'Do not build a multi-threaded version'
+  option "without-threaded", "Do not build a multi-threaded version"
 
   def patches
     # remove dependency on TINYTIM environment variable
@@ -13,22 +13,22 @@ class Tinytim < Formula
   end
 
   def install
-    if build.without? 'threaded'
-      system 'make macosx'
+    if build.without? "threaded"
+      system "make macosx"
     else
-      system 'make threadedmacosx'
+      system "make threadedmacosx"
     end
-    bin.install 'tiny1', 'tiny2', 'tiny3', 'makemaps'
+    bin.install "tiny1", "tiny2", "tiny3", "makemaps"
     doc.install %w[READ.ME README.1ST RELEASE.TXT TinyTim_WFC3.doc tinytim.pdf tinytim.doc]
-    mkdir share+'tinytim'
-    mv Dir.glob('*.pup'), share+'tinytim'
-    mv Dir.glob('*.tab'), share+'tinytim'
-    mv Dir.glob('*.fil'), share+'tinytim'
-    mv Dir.glob('*.dat'), share+'tinytim'
-    mv Dir.glob('*.fits'), share+'tinytim'
-    mv Dir.glob('*.new'), share+'tinytim'
-    mv 'cflow.graph', share+'tinytim'
-    mv 'spectra.lis', share+'tinytim'
+    mkdir share+"tinytim"
+    mv Dir.glob("*.pup"), share+"tinytim"
+    mv Dir.glob("*.tab"), share+"tinytim"
+    mv Dir.glob("*.fil"), share+"tinytim"
+    mv Dir.glob("*.dat"), share+"tinytim"
+    mv Dir.glob("*.fits"), share+"tinytim"
+    mv Dir.glob("*.new"), share+"tinytim"
+    mv "cflow.graph", share+"tinytim"
+    mv "spectra.lis", share+"tinytim"
   end
 end
 
