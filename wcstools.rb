db9c467b24772f47485a6b211af12fb4483dc8c8
Larry Bradley <larry.bradley@gmail.com>
2014-07-29 14:47:34 -0400

    Update wcstools formula

--- wcstools.rb
+++ wcstools.rb
@@ -1,14 +1,14 @@
-require 'formula'
+require "formula"
 
 class Wcstools < Formula
-  homepage 'http://tdc-www.harvard.edu/wcstools/'
-  url 'http://tdc-www.harvard.edu/software/wcstools/wcstools-3.8.7.tar.gz'
-  sha1 '412d22938b05110eb86f7b01a874c17447ad2f19'
+  homepage "http://tdc-www.harvard.edu/wcstools/"
+  url "http://tdc-www.harvard.edu/software/wcstools/wcstools-3.8.7.tar.gz"
+  sha1 "412d22938b05110eb86f7b01a874c17447ad2f19"
 
-  depends_on 'cfitsio'
+  depends_on "cfitsio"
 
   def install
-    system 'make all'
-    bin.install Dir['bin/*']
+    system "make all"
+    bin.install Dir["bin/*"]
   end
 end
