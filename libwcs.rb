7aa26f5e2a257fd3bac36c0e5fc2345364dcc50c
Larry Bradley <larry.bradley@gmail.com>
2014-07-29 14:41:53 -0400

    Update libwcs formula

--- libwcs.rb
+++ libwcs.rb
@@ -1,16 +1,16 @@
-require 'formula'
+require "formula"
 
 class Libwcs < Formula
-  homepage 'http://tdc-www.harvard.edu/wcstools/'
-  url 'http://tdc-www.harvard.edu/software/wcstools/wcstools-3.8.7.tar.gz'
-  sha1 '412d22938b05110eb86f7b01a874c17447ad2f19'
+  homepage "http://tdc-www.harvard.edu/wcstools/"
+  url "http://tdc-www.harvard.edu/software/wcstools/wcstools-3.8.7.tar.gz"
+  sha1 "412d22938b05110eb86f7b01a874c17447ad2f19"
 
-  depends_on 'cfitsio'
+  depends_on "cfitsio"
 
   def install
-    cd 'libwcs'
-    system 'make'
-    lib.install 'libwcs.a'
-    include.install Dir['*.h']
+    cd "libwcs"
+    system "make"
+    lib.install "libwcs.a"
+    include.install Dir["*.h"]
   end
 end
