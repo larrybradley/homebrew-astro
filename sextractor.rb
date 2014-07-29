91d72fc27f626c2fffe092258f6ec3e9e43b2bac
Larry Bradley <larry.bradley@gmail.com>
2014-07-29 16:07:29 -0400

    Add sextractor 2.8.6 formula

--- sextractor.rb
+++ sextractor.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class Sextractor286 < Formula
+class Sextractor < Formula
   homepage "https://www.astromatic.net/software/sextractor"
   url "https://www.astromatic.net/download/sextractor/sextractor-2.8.6.tar.gz"
   sha1 "103ac2d51d9bae9fcbc5dda3031d82cd127f8250"
@@ -15,6 +15,7 @@ class Sextractor286 < Formula
     "https://gist.github.com/larrybradley/9ab242c100b5227168c9/raw/630f75504fe6fb09fd98cc9acd8119293445c504/sextractor286.patch"
   end
 
+
   def install
     args = %W[
       --prefix=#{prefix}
@@ -28,12 +29,6 @@ class Sextractor286 < Formula
     system "./configure", *args
     system "make"
     system "make install"
-
-    File.rename(bin+"sex", bin+"sex286")
-    File.rename(bin+"ldactoasc", bin+"ldactoasc286")
-    File.rename(man1+"sex.1", man1+"sex286.1")
-    rm man+"manx/sex.x"
-    File.rename(share+"sextractor", share+"sextractor286")
-    (share+'sextractor286').install Dir["config/*"]
+    mv Dir["config/*"], share+"sextractor"
   end
 end
