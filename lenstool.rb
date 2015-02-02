b7cf673eccdd3596352435ec41604f09f98af0d7
Larry Bradley <larry.bradley@gmail.com>
2015-02-02 14:29:35 -0500

    Update lenstool formula

--- lenstool.rb
+++ lenstool.rb
@@ -2,33 +2,33 @@ require "formula"
 
 class Lenstool < Formula
   homepage "http://projets.lam.fr/projects/lenstool/wiki"
-  url "http://projets.lam.fr/attachments/download/348/lenstool_v6.7.1.tar.gz"
-  sha1 "9f4a97f7099c8bb70d8a64da2da70c3770b88320"
-  #version "6.7.1"
+  url "http://projets.lam.fr/attachments/download/1345/lenstool-6.8.tar.gz"
+  sha1 "3c71b7cf62dcbd2ef9cd351f651f0cc9c5dcaa87"
 
   depends_on "libwcs"
   depends_on "cfitsio"
   depends_on "gsl"
-  depends_on "pgplot"
+  depends_on "pgplot" => :optional
 
   def install
     args = %W[
       --prefix=#{prefix}
+      --with-cfitsio-lib-path=#{HOMEBREW_PREFIX}/lib
+      --with-cfitsio-include-path=#{HOMEBREW_PREFIX}/include
+      --with-wcslib-lib-path=#{HOMEBREW_PREFIX}/lib
+      --with-wcslib-include-path=#{HOMEBREW_PREFIX}/include
+      --with-gsl-lib-path=#{HOMEBREW_PREFIX}/lib
+      --with-gsl-include-path=#{HOMEBREW_PREFIX}/include
+      --disable-openmp
     ]
 
     system "./configure", *args
     system "make"
     system "make install"
 
-    #mkdir share
-    #mkdir share+"lenstool"
-    #mv "examples", share+"lenstool"
-    #mv "examples_table", share+"lenstool"
-    #mv "perl", share+"lenstool"
     (share+"lenstool").install Dir["examples"]
     (share+"lenstool").install Dir["examples_table"]
     (share+"lenstool").install Dir["perl"]
-
     doc.install Dir["doc/*"]
   end
 end
