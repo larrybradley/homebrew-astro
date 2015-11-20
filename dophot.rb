cb33645a10a6e4438aabd33c15fc1742d01d6258
Larry Bradley <larry.bradley@gmail.com>
2015-11-20 10:55:48 -0500

    Add DoPhot

---
+++ dophot.rb
@@ -0,0 +1,24 @@
+require "formula"
+
+class Dophot < Formula
+  homepage "https://github.com/M1TDoPHOT/DoPHOT_C"
+  url "http://www.stsci.edu/~lbradley/homebrew/dophot-4.2.tar.gz"
+  sha256 "4f332ed0aa3b5f8dd48785db4a2e949d7925a3c400e1cab03b78fa4c40042031"
+  version "4.2"
+
+  depends_on "cfitsio"
+
+  patch do
+    # fix Makefile paths
+    url "https://gist.githubusercontent.com/larrybradley/61ca756641cca8696910/raw/1329c11ed1148684834d0bf66e695774d420eb74/dophot_makefile.patch"
+    sha256 "5f1724045af8f0210f0d20c07d282d80ad41bddbc53e44669b4da7e805b0b4b5"
+  end
+
+  def install
+    system "make", "test"
+    system "make", "dophot"
+    bin.install "working_data/dophot"
+    (share+"dophot").install "working_data/param_default_c"
+    (share+"dophot").install "verif_data/pm"
+  end
+end
