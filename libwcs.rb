fe754080f3d831b1efcdb4559a67780cbfe7b1f4
Larry Bradley <larry.bradley@gmail.com>
2017-04-24 14:25:42 -0400

    Update libwcs

--- libwcs.rb
+++ libwcs.rb
@@ -3,8 +3,8 @@ require "formula"
 class Libwcs < Formula
   desc "A library for using World Coordinate Systems (WCS) in astronomical images"
   homepage "http://tdc-www.harvard.edu/wcstools/"
-  url "http://tdc-www.harvard.edu/software/wcstools/wcstools-3.9.3.tar.gz"
-  sha256 "1005ae353e37c498490163f4785705fdb8bb3633dccb6291bf3b24b2135ab949"
+  url "http://tdc-www.harvard.edu/software/wcstools/wcstools-3.9.5.tar.gz"
+  sha256 "b2f9be55fdec29f0c640028a9986771bfd6ab3d2f633953e4c7cc3b410e5fe9c"
   depends_on "cfitsio"
 
   def install
