8757c6788699b10a2e9fa7d402db043e7f55a823
Larry Bradley <larry.bradley@gmail.com>
2017-09-22 10:56:07 -0400

    Update match

--- match.rb
+++ match.rb
@@ -3,8 +3,8 @@ require "formula"
 class Match < Formula
   desc "Match items in two different lists"
   homepage "http://spiff.rit.edu/match/"
-  url "http://spiff.rit.edu/match/match-0.16.tar.gz"
-  sha256 "f865e91e7c9b26147ca0215db559df1f17354e374d81627975cd98cc5020eff8"
+  url "http://spiff.rit.edu/match/match-1.0.tar.gz"
+  sha256 "2f1e408cc3458e88f0ec7bc54554883addbf1a84a2cea63c78d48a82e28228f3"
 
   def install
     args = %W[
