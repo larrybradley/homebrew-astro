e0779d8ea4a29a304da358cee1f37be44e26876d
Larry Bradley <larry.bradley@gmail.com>
2018-05-15 20:06:23 -0400

    Update ds9

--- ds9.rb
+++ ds9.rb
@@ -4,20 +4,20 @@ class Ds9 < Formula
   desc "Astronomical imaging and data visualization"
   homepage "http://ds9.si.edu/"
 
-  if MacOS.version == :el_capitan
+  if MacOS.version == :sierra
     stable do
-      url "http://ds9.si.edu/download/darwinelcapitan/ds9.darwinelcapitan.7.5.tar.gz"
-      sha256 "52506e849bd9967a9e43c449357e72d7371fee2eeef65b5f7868c9c8cbb96081"
-      version "7.5"
+      url "http://ds9.si.edu/download/darwinsierra/ds9.darwinsierra.7.6.tar.gz"
+      sha256 "e0b67fee1b61ae8e73e09dc0479a266371b8aebeb9b536b33a6aa9c2c2a6ce7d"
+      version "7.6"
     end
-  elsif MacOS.version == :sierra
+  elsif MacOS.version == :high_sierra
     stable do
-      url "http://ds9.si.edu/download/darwinsierra/ds9.darwinsierra.7.5.tar.gz"
-      sha256 "2509b495370af3b4edfe567e7f21d426910cea7f23b9a229894e83008ea2ddbc"
-      version "7.5"
+      url "http://ds9.si.edu/download/darwinhighsierra/ds9.darwinhighsierra.7.6.tar.gz"
+      sha256 "9a3fbfc1718a434cf5b677e1c34839bff7f9d2b2ee398b9a9a42e5c54ef84255"
+      version "7.6"
     end
   else
-    onoe "This formula is only for Yosemite and above"
+    onoe "ds9 - This formula is for Sierra and High Sierra only."
   end
 
   def install
