4f7c236f8b7d90ff0e0d0288941bd27675b38438
Larry Bradley <larry.bradley@gmail.com>
2020-06-08 22:36:04 -0400

    Add ds9 for catalina

--- ds9.rb
+++ ds9.rb
@@ -2,28 +2,32 @@ require "formula"
 
 class Ds9 < Formula
   desc "Astronomical imaging and data visualization"
-  homepage "http://ds9.si.edu/"
+  homepage "https://sites.google.com/cfa.harvard.edu/saoimageds9/home"
 
-  if MacOS.version == :sierra
+
+  if MacOS.version == :catalina
     stable do
-      url "http://ds9.si.edu/download/darwinsierra/ds9.darwinsierra.8.0.1.tar.gz"
-      sha256 "8353a1e3f54014e46ab822825f25c978c61f1cfd66e3fbe1591ef794ba82d2c0"
-      version "8.0.1"
+      url "https://ds9.si.edu/download/darwincatalina/ds9.darwincatalina.8.1.tar.gz"
+      sha256 "53664d3d24028d3dc098362d826935e192166897349aec8cd3906116a31963c5"
+      version "8.1"
     end
-  elsif MacOS.version == :high_sierra
-    stable do
-      url "http://ds9.si.edu/download/darwinhighsierra/ds9.darwinhighsierra.8.0.1.tar.gz"
-      sha256 "c43c68e8ae3147f611a5ff2c6f6464e0046fcd3e652aa07faf047c8095286dc4"
-      version "8.0.1"
-    end
+
   elsif MacOS.version == :mojave
     stable do
-      url "http://ds9.si.edu/download/darwinmojave/ds9.darwinmojave.8.0.1.tar.gz"
-      sha256 "b9fd02afa975dd5376530086e3b19c7b475d331ddb6af9ba44a064db09e296b4"
-      version "8.0.1"
+      url "https://ds9.si.edu/download/darwinmojave/ds9.darwinmojave.8.1.tar.gz"
+      sha256 "cd6563e1b851463074999536a661ed0342a7127cad30ef4fbe77fa413891a543"
+      version "8.1"
     end
+
+  elsif MacOS.version == :high_sierra
+    stable do
+      url "https://ds9.si.edu/download/darwinhighsierra/ds9.darwinhighsierra.8.0.1.tar.gz"
+      sha256 "a9da274428560d988bc13e4c187ed9b035e5358aa0f6ae03b3ad33c185af0b27"
+      version "8.1"
+    end
+
   else
-    onoe "ds9 - This formula is for OSX 10.12 (Sierra) or higher."
+    onoe "ds9 - This formula is for OSX 10.13 (High Sierra) or higher."
   end
 
   def install
