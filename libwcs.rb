c6046e4a21a7d6b5db1b44db835744c1eb62a843
Larry Bradley <larry.bradley@gmail.com>
2021-08-16 17:39:06 -0400

    Update wcstools sha256

--- libwcs.rb
+++ libwcs.rb
@@ -4,7 +4,7 @@ class Libwcs < Formula
   desc "A library for using World Coordinate Systems (WCS) in astronomical images"
   homepage "http://tdc-www.harvard.edu/wcstools/"
   url "http://tdc-www.harvard.edu/software/wcstools/wcstools-3.9.6.tar.gz"
-  sha256 "d99a4c114ee48d0806f3693b003b673446088104a30ecc95cd27ebbe2a704ec5"
+  sha256 "184e8a1377373e8dfeaa19b438771d8140126d5510cba55dbf86ceac98e7aff5"
   depends_on "cfitsio"
 
   def install
