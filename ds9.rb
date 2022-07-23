cc913c63b9aa7938b50f212cd280aa35b5ddb1a5
Larry Bradley <larry.bradley@gmail.com>
2022-07-22 20:36:52 -0400

    Update ds9 formula

--- ds9.rb
+++ ds9.rb
@@ -4,7 +4,17 @@ class Ds9 < Formula
   desc "Astronomical imaging and data visualization"
   homepage "https://sites.google.com/cfa.harvard.edu/saoimageds9/home"
 
-  if MacOS.version == :big_sur
+  if MacOS.version == :monterey
+    stable do
+      onoe "ds9 v8.3 is not available for monterey"
+    end
+
+  elsif MacOS.version == :amd64_monterey
+    stable do
+      onoe "ds9 v8.3 is not available for monterey AMD"
+    end
+
+  elsif MacOS.version == :big_sur
     stable do
       url "https://ds9.si.edu/download/darwinbigsur/ds9.darwinbigsurx86.8.3.tar.gz"
       sha256 "81883d4c3dd5166f74ba47eca4892c6e58ec86a304863b8b01893c1d53f89ee2"
