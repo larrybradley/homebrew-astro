59950e827e8673a8bba5397d073c6b152395159f
Larry Bradley <larry.bradley@gmail.com>
2024-03-13 10:37:07 -0400

    Update wcstools sha256 due to new 3.9.7 tarball

--- wcstools.rb
+++ wcstools.rb
@@ -4,7 +4,7 @@ class Wcstools < Formula
   desc "Tools for using World Coordinate Systems (WCS) in astronomical images"
   homepage "http://tdc-www.harvard.edu/wcstools/"
   url "http://tdc-www.harvard.edu/software/wcstools/wcstools-3.9.7.tar.gz"
-  sha256 "efebf6970f6158ed002fdfe320d4ff4738e3a049b333a749da6cc464b3579860"
+  sha256 "525f6970eb818f822db75c1526b3122b1af078affa572dce303de37df5c7b088"
   depends_on "cfitsio"
 
   def install
