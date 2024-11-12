9eedad7540954b7cd7805b7926ad678b7ddc0e4e
Larry Bradley <larry.bradley@gmail.com>
2024-11-11 22:18:22 -0500

    Update ds9 to 8.6.0

--- ds9.rb
+++ ds9.rb
@@ -6,30 +6,23 @@ class Ds9 < Formula
 
   if MacOS.version == :sonoma
     stable do
-      url "https://ds9.si.edu/download/darwinventurax86/ds9.darwinventurax86.8.5.tar.gz"
-      sha256 "1808bcf3bf9ae869e3aa67dd0f51d16791026f6a63de1269f25a90f57972f189"
-      version "8.5.0"
-    end
-
-  elsif MacOS.version == :amd64_sonoma
-    stable do
-      url "https://ds9.si.edu/download/darwinventuraarm64/ds9.darwinventuraarm64.8.5.tar.gz"
-      sha256 "f933ba18e75feeb4e2e53365ea412de9d75ae6431883b9030a6514d996854ef0"
-      version "8.5.0"
-    end
-
-  elsif MacOS.version == :ventura
-    stable do
-      url "https://ds9.si.edu/download/darwinventurax86/ds9.darwinventurax86.8.5.tar.gz"
-      sha256 "1808bcf3bf9ae869e3aa67dd0f51d16791026f6a63de1269f25a90f57972f189"
-      version "8.5.0"
+      url "https://ds9.si.edu/download/darwinsonomaarm64/ds9.darwinsonomaarm64.8.6.tar.gz"
+      sha256 "aa994cdf49e220852f5483cf24e29147590c05a43bdefa14f2415bb249f623de"
+      version "8.6.0"
     end
 
   elsif MacOS.version == :amd64_ventura
     stable do
-      url "https://ds9.si.edu/download/darwinventuraarm64/ds9.darwinventuraarm64.8.5.tar.gz"
-      sha256 "f933ba18e75feeb4e2e53365ea412de9d75ae6431883b9030a6514d996854ef0"
-      version "8.5.0"
+      url "https://ds9.si.edu/download/darwinventuraarm64/ds9.darwinventuraarm64.8.6.tar.gz"
+      sha256 "2d7895510ce6772246b15dc83b31ec5e26ac21333cd2aaddc9fb3968ee96ca6a"
+      version "8.6.0"
+    end
+
+  elsif MacOS.version == :ventura
+    stable do
+      url "https://ds9.si.edu/download/darwinventurax86/ds9.darwinventurax86.8.6.tar.gz"
+      sha256 "ffd53215d0d6a12d01d7d5435e6b59199f148c0810354aa2331123474f141980"
+      version "8.6.0"
     end
 
   else
