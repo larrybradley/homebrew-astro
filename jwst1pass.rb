eb86be9b2524238a179473cefff89c948e86a8f2
Larry Bradley <larry.bradley@gmail.com>
2025-09-30 12:44:52 -0400

    Update jwst1pass

--- jwst1pass.rb
+++ jwst1pass.rb
@@ -1,13 +1,13 @@
 class Jwst1pass < Formula
   desc "Fit ePSFs to JWST images"
   homepage "https://www.stsci.edu/~jayander/JWST1PASS/"
-  url "https://www.stsci.edu/~jayander/JWST1PASS/CODE/jwst1pass.2025.09.26_v1e.F"
-  sha256 "41410fe432cf28f1a51d69a379ef59195aa8fa1705d8017b937aa9737fa746b8"
+  url "https://www.stsci.edu/~jayander/JWST1PASS/CODE/jwst1pass.2025.09.30_v1f.F"
+  sha256 "1f59e9726306fe32ee4bb3266e92ce4beeb30b19935582d273cb1cd75767af2a"
 
   depends_on "gcc"  # for gfortran
 
   def install
-    system "gfortran", "jwst1pass.2025.09.26_v1e.F", "-o", "jwst1pass"
+    system "gfortran", "jwst1pass.2025.09.30_v1f.F", "-o", "jwst1pass"
     bin.install "jwst1pass"
   end
 
