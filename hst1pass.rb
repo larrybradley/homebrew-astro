b9e27f23f4858279e1493d02deaf25a85c592b60
Larry Bradley <larry.bradley@gmail.com>
2026-07-17 12:58:27 -0400

    Update hst1pass version

--- hst1pass.rb
+++ hst1pass.rb
@@ -1,13 +1,13 @@
 class Hst1pass < Formula
   desc "Fit ePSFs to HST images"
   homepage "https://www.stsci.edu/~jayander/HST1PASS/"
-  url "https://www.stsci.edu/~jayander/HST1PASS/CODE/hst1pass/hst1pass.2025.02.14_v1h.F"
-  sha256 "5f49dacbd5ae47bee71abd83567e7cbfc25b7c72f63743f28c41e632424ea1db"
+  url "https://www.stsci.edu/~jayander/HST1PASS/CODE/hst1pass/hst1pass.2026.03.24_v2b.F"
+  sha256 "5467d81fffc775f43382c1cf13c499592159575f330eb4591b997c01edb21887"
 
   depends_on "gcc"  # for gfortran
 
   def install
-    system "gfortran", "hst1pass.2025.02.14_v1h.F", "-o", "hst1pass"
+    system "gfortran", "hst1pass.2026.03.24_v2b.F", "-o", "hst1pass"
     bin.install "hst1pass"
   end
 
