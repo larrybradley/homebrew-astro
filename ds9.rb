1519024bfef46c8adfa4e26fa7b778cf7c06eac3
Larry Bradley <larry.bradley@gmail.com>
2026-07-17 12:54:26 -0400

    Update ds9

--- ds9.rb
+++ ds9.rb
@@ -4,29 +4,29 @@ class Ds9 < Formula
   url "file:///dev/null"
   # url "https://ds9.si.edu/download/source/"
 
-  if MacOS.version == :tahoe || MacOS.version == :sequoia || MacOS.version == :sonoma
+  if MacOS.version == :tahoe
     stable do
-      url "https://ds9.si.edu/download/darwinsonomaarm64/ds9.darwinsonomaarm64.8.6.tar.gz"
-      sha256 "aa994cdf49e220852f5483cf24e29147590c05a43bdefa14f2415bb249f623de"
-      version "8.6.0"
+      url "https://ds9.si.edu/download/darwintahoearm64/ds9.darwintahoearm64.8.7.tar.gz"
+      sha256 "48bd1f0510c45341b6a5cfb0642ea311c3aa9e00e082b8a45c8e01b0dd454a66"
+      version "8.7"
     end
 
-  elsif MacOS.version == :amd64_ventura
+  elsif MacOS.version == :sequoia
     stable do
-      url "https://ds9.si.edu/download/darwinventuraarm64/ds9.darwinventuraarm64.8.6.tar.gz"
-      sha256 "2d7895510ce6772246b15dc83b31ec5e26ac21333cd2aaddc9fb3968ee96ca6a"
-      version "8.6.0"
+      url "https://ds9.si.edu/download/darwinsequoiaarm64/ds9.darwinsequoiaarm64.8.7.tar.gz"
+      sha256 "014c33378d262958660b398aba01282e0b8bcf547d50fe29b902df3b94030fcf"
+      version "8.7"
     end
 
-  elsif MacOS.version == :ventura
+  elsif MacOS.version == :sonoma
     stable do
-      url "https://ds9.si.edu/download/darwinventurax86/ds9.darwinventurax86.8.6.tar.gz"
-      sha256 "ffd53215d0d6a12d01d7d5435e6b59199f148c0810354aa2331123474f141980"
-      version "8.6.0"
+      url "https://ds9.si.edu/download/darwinsonomaarm64/ds9.darwinsonomaarm64.8.7.tar.gz"
+      sha256 "0141e33117c32c77d305ba5647cbc5f1ef244a3caaf92ec4dc18b0c8b5b1efc5"
+      version "8.7"
     end
 
   else
-    onoe "ds9 - This formula is for macOS Ventura or higher."
+    onoe "ds9 - This formula is for macOS Sonoma or higher."
   end
 
   def install
